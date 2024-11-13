b39eccf499ccd51c30c144560ee163043e36cac1
Sameer Ahmed <balochtradersnayapay@gmail.com>
2024-11-13 20:02:45 +0500

    added stop function

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var green = document.getElementById("green");
 var start = document.getElementById("ready");
 var run = document.getElementById("run");
 
-var go = gsap.to(car, { x: -1400, duration: 5, ease: "none" }).repeat(Infinity);
+// var go = gsap.to(car, { x: -1400, duration: 5, ease: "none" }).repeat(Infinity);
 
 // function for moving car
 function move() {
   green.style.backgroundColor = "rgb(33, 255, 81)";
   run.play();
-  gsap.to(car, { x: -1400, duration: 5 }).repeat(Infinity);
+  go = gsap.to(car, { x: -1400, duration: 5, ease: "none" }).repeat(Infinity);
 }
 
 // for yellow light
@@ -20,8 +20,8 @@ function ready() {
   start.play();
 }
 
-
 // for red
-function toStop(){
-  
+function toStop() {
+  red.style.backgroundColor = "rgb(255, 67, 67)";
+  gsap.killTweensOf(car);
 }
