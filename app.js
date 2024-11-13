68377b0d4fb9526ba41410707512251984593029
Sameer Ahmed <balochtradersnayapay@gmail.com>
2024-11-13 19:15:12 +0500

    added functionality

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ var car = document.getElementById("car-img");
 var red = document.getElementById("red");
 var yellow = document.getElementById("yellow");
 var green = document.getElementById("green");
+var start = document.getElementById("ready");
+var run = document.getElementById("run");
 
+var go = gsap.to(car, { x: -1400, duration: 5, ease: "none" }).repeat(Infinity);
+
+// function for moving car
 function move() {
-  gsap
-    .fromTo(car, { x: -10 }, { x: -1400, ease: "none", duration: 6 })
-    .repeat(Infinity);
-  //   gsap.to(car, { x: -1400, duration: 5, ease: "none" }).repeat(Infinity);
+  green.style.backgroundColor = "rgb(33, 255, 81)";
+  run.play();
+  gsap.to(car, { x: -1400, duration: 5 }).repeat(Infinity);
+}
+
+// for yellow light
+function ready() {
+  yellow.style.backgroundColor = "rgb(251, 255, 2)";
+  start.play();
+}
+
+
+// for red
+function toStop(){
+  
 }
